a1d82ba2b3ab1fbd341776ba4722bc2a4bdd11ff
Hoang-Minh <quataoxanh83@gmail.com>
2015-02-15 20:51:25 -0800

    Triangle 4 Added Right Triangle Correct Version

--- Triangle3/Triangle3/Source.cpp
+++ Triangle3/Triangle3/Source.cpp
@@ -13,47 +13,49 @@ using namespace std;
 
 int main()
 {
-    int a, b, c;
-    bool c1, c2, c3, IsATriangle;
+	int a, b, c;
+	bool c1, c2, c3, IsATriangle;
 
-    do
+	do
 	{
-        cout << "Enter 3 integers which are sides of a triangle" << endl;
-        cin >> a;
-        cin >> b;
-        cin >> c;
+		cout << "Enter 3 integers which are sides of a triangle" << endl;
+		cin >> a;
+		cin >> b;
+		cin >> c;
 
-        c1 = (1 <= a) && (a <= 300);
-        c2 = (1 <= b) && (b <= 300);
-        c3 = (1 <= c) && (c <= 300);
+		c1 = (1 <= a) && (a <= 300);
+		c2 = (1 <= b) && (b <= 300);
+		c3 = (1 <= c) && (c <= 300);
 
-        if(!c1)
-            cout << "Value of a is not in the range of permitted values" << endl;
-        if(!c2)
-            cout << "Value of b is not in the range of permitted values" << endl;
-        if(!c3)
-            cout << "Value of c is not in the range of permitted values" << endl;
-	}while(!c1 || !c2 || !c3);
-    
-    cout << "Side A is " << a << endl;
-    cout << "Side B is " << b << endl;
-    cout << "Side C is " << c << endl;
+		if (!c1)
+			cout << "Value of a is not in the range of permitted values" << endl;
+		if (!c2)
+			cout << "Value of b is not in the range of permitted values" << endl;
+		if (!c3)
+			cout << "Value of c is not in the range of permitted values" << endl;
+	} while (!c1 || !c2 || !c3);
 
-    if((a < b + c) && (b < a + c) && (c < a + b))
-        IsATriangle = true;
+	cout << "Side A is " << a << endl;
+	cout << "Side B is " << b << endl;
+	cout << "Side C is " << c << endl;
+
+	if ((a < b + c) && (b < a + c) && (c < a + b))
+		IsATriangle = true;
 	else
-        IsATriangle = false;
-    
-    if(IsATriangle)
+		IsATriangle = false;
+
+	if (IsATriangle)
 	{
-        if((a == b) && (b == c))
-            cout << "Equilateral" << endl;
-		else if ((c * c == a * a + b * b) || (a * a == c * c + b * b) || (b * b == a * a + c * c))
-			cout << "Right triangle" << endl;
-		else if ((a != b) && (a != c) && (b != c))
-			cout << "Scalene" << endl;		
+		if ((a == b) && (b == c))
+			cout << "Equilateral" << endl;
+		else if ((a == b || a == c || b == c) && (c * c == a * a + b * b) || (a * a == c * c + b * b) || (b * b == a * a + c * c))
+			cout << "Right Isosceles" << endl;
+		else if (a == b || a == c || b == c)
+			cout << "Isosceles" << endl;
+		else if ((a * a == c * c + b * b) || (b * b == a * a + c * c) || (c * c == a * a + b * b))
+			cout << "Right" << endl;
 		else
-            cout << "Isosceles" << endl;
+			cout << "Scalene" << endl;		
 	}
 	else
         cout << "Not a Triangle" << endl;
